f0b0ac9cc7ee0d0ebeda663863c6e8505a850ea5
Adi-Ram123 <74124497+Adi-Ram123@users.noreply.github.com>
2023-03-24 09:02:59 -0500

    Fixed double input
    Need to fix animation transitions

--- Assets/Top Down Adventure Assets/Visuals/CHARACTERS/Walking/Movement.cs
+++ Assets/Top Down Adventure Assets/Visuals/CHARACTERS/Walking/Movement.cs
@@ -7,11 +7,12 @@ public class Movement : MonoBehaviour
     [SerializeField] float speed;
     private Animator anime;
     private Rigidbody2D rb;
-    private bool up, down, left, right, walk;
+    private Vector2 move;
+    private bool up, down, left, right, walk, pressed;
     // Start is called before the first frame update
     void Start()
     {
-        up = down = left = right = walk = false;
+        up = down = left = right = walk = pressed = false;
         anime = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -19,66 +20,85 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+
+
+        if (!pressed && Input.GetKeyDown(KeyCode.A))
         {
             left = true;
             walk = true;
+            pressed = true;
+            move.x = Input.GetAxisRaw("Horizontal");
+
         }
-            
+
         if (Input.GetKeyUp(KeyCode.A))
         {
             left = false;
             walk = false;
+            pressed = false;
             anime.ResetTrigger("Left");
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (!pressed && Input.GetKeyDown(KeyCode.D))
         {
             right = true;
             walk = true;
+            pressed = true;
+            move.x = Input.GetAxisRaw("Horizontal");
+
         }
 
         if (Input.GetKeyUp(KeyCode.D))
         {
             right = false;
             walk = false;
+            pressed = false;
             anime.ResetTrigger("Right");
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (!pressed && Input.GetKeyDown(KeyCode.W))
         {
             up = true;
             walk = true;
+            pressed = true;
+            move.y = Input.GetAxisRaw("Vertical");
+
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             up = false;
             walk = false;
+            pressed = false;
             anime.ResetTrigger("Up");
 
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (!pressed && Input.GetKeyDown(KeyCode.S))
         {
             down = true;
             walk = true;
+            pressed = true;
+            move.y = Input.GetAxisRaw("Vertical");
+
         }
 
         if (Input.GetKeyUp(KeyCode.S))
         {
             down = false;
             walk = false;
+            pressed = false;
             anime.ResetTrigger("Down");
 
         }
-
-
 
     }
 
     private void FixedUpdate()
     {
+
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+
         if (walk)
         {
             anime.SetBool("Walk", true);
@@ -87,34 +107,34 @@ public class Movement : MonoBehaviour
         else
             anime.SetBool("Walk", false);
 
-        if(up)
+        if (up)
         {
             anime.SetTrigger("Up");
-            rb.velocity = new Vector2(0, speed * Time.deltaTime);
+            //rb.velocity = new Vector2(0, speed * Time.deltaTime);
         }
 
         if (right)
         {
             anime.SetTrigger("Right");
-            rb.velocity = new Vector2(speed * Time.deltaTime, 0);
+            //rb.velocity = new Vector2(speed * Time.deltaTime, 0);
         }
 
         if (down)
         {
             anime.SetTrigger("Down");
-            rb.velocity = new Vector2(0, -speed * Time.deltaTime);
+           //rb.velocity = new Vector2(0, -speed * Time.deltaTime);
         }
 
         if (left)
         {
             anime.SetTrigger("Left");
-            rb.velocity = new Vector2(-speed * Time.deltaTime, 0);
+            //rb.velocity = new Vector2(-speed * Time.deltaTime, 0);
         }
 
-
-        if (!up && !right && !left && !down)
-            rb.velocity = new Vector2(0, 0);
-
+        if (!up && !right && !down && !left)
+        {
+            move = new Vector2(0, 0);
+        }
     }
 
 }
