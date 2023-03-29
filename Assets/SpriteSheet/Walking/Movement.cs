f8e97e55139c5876e6e16a0c89e4aaacf59585b5
Hrudith <69597791+HrudithL@users.noreply.github.com>
2023-03-29 11:03:27 -0500

    Revert "Revert "Revert "Added All the Rule Tiles"""
    This reverts commit 33765fe28a1f035aa0fc67d2112356d3b2930708.

---
+++ Assets/SpriteSheet/Walking/Movement.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private Animator anime;
+    private Rigidbody2D rb;
+    private Vector2 move;
+    private bool walk, pressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!pressed && Input.GetKeyDown(KeyCode.A))
+        {
+            walk = true;
+            pressed = true;
+            move.x = Input.GetAxisRaw("Horizontal");
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            walk = false;
+            pressed = false;
+        }
+
+        if (!pressed && Input.GetKeyDown(KeyCode.D))
+        {
+            walk = true;
+            pressed = true;
+            move.x = Input.GetAxisRaw("Horizontal");
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            walk = false;
+            pressed = false;
+        }
+
+        if (!pressed && Input.GetKeyDown(KeyCode.W))
+        {
+            walk = true;
+            pressed = true;
+            move.y = Input.GetAxisRaw("Vertical");
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            walk = false;
+            pressed = false;
+
+        }
+
+        if (!pressed && Input.GetKeyDown(KeyCode.S))
+        {
+            walk = true;
+            pressed = true;
+            move.y = Input.GetAxisRaw("Vertical");
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            walk = false;
+            pressed = false;
+
+        }
+
+    }
+
+   
+
+    private void FixedUpdate()
+    {
+        if (move != Vector2.zero)
+        {
+            anime.SetFloat("xInput", move.x);
+            anime.SetFloat("yInput", move.y);
+        }
+
+        if (walk)
+        {
+            anime.SetBool("Walk", true);
+            rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        }
+
+        else
+        {
+            anime.SetBool("Walk", false);
+            move = Vector2.zero;
+
+        }
+
+    }
+
+}
