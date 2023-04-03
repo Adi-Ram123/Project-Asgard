24e9143f46fae31255a340fdc5e7f345c494da6b
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2023-04-03 10:02:47 -0500

    Progress on coding battle

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
