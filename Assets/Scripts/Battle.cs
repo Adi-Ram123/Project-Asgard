7771d9dd533eb5fb8eb1715ad1a9c3a88c2ba000
vrnayak20 <76406392+vrnayak20@users.noreply.github.com>
2023-04-05 09:01:30 -0500

    Update Battle.cs

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] GameObject mainCanvas;
+    [SerializeField] GameObject fightCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fightCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(Keycode.Escape))
+        {
+            // set all other canvases inactive
+            mainCanvas.SetActive(true);
+        }
+
     }
 
     void FixedUpdate()
     {
         if (Options.selected.Equals("Fight"))
         {
-
+            mainCanvas.SetActive(false);
+            fightCanvas.SetActive(true);
+            Options.selected = "";
         }
     }
 }
